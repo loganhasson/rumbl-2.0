7aae42f04860abc1e23300db33ea6fcbd7680883
Logan Hasson <logan.hasson@gmail.com>
2016-01-13 19:24:06 -0500

    Probably should start committing this

---
+++ web/models/category.ex
@@ -0,0 +1,28 @@
+defmodule Rumbl.Category do
+  use Rumbl.Web, :model
+
+  schema "categories" do
+    field :name, :string
+    has_many :videos, Rumbl.Video
+
+    timestamps
+  end
+
+  @required_fields ~w(name)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+
+  def alphabetical(query \\ __MODULE__) do
+    from c in query, order_by: c.name
+  end
+end
