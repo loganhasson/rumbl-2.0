182dd8316ff4380192c9661a9794a1adb36e0723
Logan Hasson <logan.hasson@gmail.com>
2016-01-13 23:54:48 -0500

    Auth tests

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -47,7 +47,7 @@ defmodule Rumbl.Auth do
   end
 
   def authenticate_user(conn, _opts) do
-    if conn.assigns.current_user do
+    if conn.assigns[:current_user] do
       conn
     else
       conn
